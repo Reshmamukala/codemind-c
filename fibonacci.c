7e00c4f6a235f7d7b49692add5f309ddaca9847a
Reshmamukala <119743065+Reshmamukala@users.noreply.github.com>
2022-12-18 21:32:03 +0530

    #include<stdio.h> int main() {     int n,i,t1=0,t2=1,n1;     scanf("%d",&n);     n1=t1+t2;     printf("%d %d ",t1,t2);     for(i=3;i<=n;i++)     {         printf("%d ",n1);         t1=t2;         t2=n1;         n1=t1+t2;     } }

---
+++ fibonacci.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,t1=0,t2=1,n1;
+    scanf("%d",&n);
+    n1=t1+t2;
+    printf("%d %d ",t1,t2);
+    for(i=3;i<=n;i++)
+    {
+        printf("%d ",n1);
+        t1=t2;
+        t2=n1;
+        n1=t1+t2;
+    }
+}
